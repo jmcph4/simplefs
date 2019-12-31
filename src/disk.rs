29a5de8fc526da7ddadf6ad7982fd6110e916c7f
jmcph4 <jmcph4@users.noreply.github.com>
2019-12-31 17:24:32 +1000

    Add missing references to `self` in traits
    This commit adds missing references to `self` in the traits for both
    the disk emulator and the filesystem itself.

--- src/disk.rs
+++ src/disk.rs
@@ -3,16 +3,17 @@ pub enum DiskError {}
 
 pub const BLOCK_SIZE: usize = 4096;
 
-pub trait Disk {
-    fn open(path: String, num_blocks: usize) -> Result<(), DiskError>;
+pub trait Disk where Self: Sized{
+    fn open(path: String, num_blocks: usize) -> Result<Self, DiskError>;
     
-    fn size() -> usize;
+    fn size(&self) -> usize;
 
-    fn mounted() -> bool;
-    fn mount() -> Result<(), DiskError>;
-    fn unmount() -> Result<(), DiskError>;
+    fn mounted(&self) -> bool;
+    fn mount(&mut self) -> Result<(), DiskError>;
+    fn unmount(&mut self) -> Result<(), DiskError>;
 
-    fn read(block_number: usize) -> Result<Vec<u8>, DiskError>;
-    fn write(block_number: usize, data: Vec<u8>) -> Result<(), DiskError>;
+    fn read(&mut self, block_number: usize) -> Result<Vec<u8>, DiskError>;
+    fn write(&mut self, block_number: usize, data: Vec<u8>) ->
+        Result<(), DiskError>;
 }
 
--- src/fs.rs
+++ src/fs.rs
@@ -3,18 +3,18 @@ use crate::disk::Disk;
 
 pub enum FileSystemError {}
 
-pub trait FileSystem<T: Disk> {
-    fn format(disk: T) -> Result<bool, FileSystemError>;
+pub trait FileSystem<T: Disk> where Self: Sized {
+    fn format(disk: T) -> Result<(), FileSystemError>;
 
-    fn mount(disk: T) -> Result<bool, FileSystemError>;
+    fn mount(disk: T) -> Result<Self, FileSystemError>;
 
-    fn create() -> Result<usize, FileSystemError>;
-    fn remove(inumber: usize) -> Result<bool, FileSystemError>;
-    fn stat(inumber: usize) -> Result<usize, FileSystemError>;
+    fn create(&mut self) -> Result<usize, FileSystemError>;
+    fn remove(&mut self, inumber: usize) -> Result<bool, FileSystemError>;
+    fn stat(&self, inumber: usize) -> Result<usize, FileSystemError>;
 
-    fn read(inumber: usize, data: &mut Vec<u8>, offset: usize) ->
+    fn read(&mut self, inumber: usize, data: &mut Vec<u8>, offset: usize) ->
         Result<usize, FileSystemError>;
-    fn write(inumber: usize, data: Vec<u8>, offset: usize) ->
+    fn write(&mut self, inumber: usize, data: Vec<u8>, offset: usize) ->
         Result<usize, FileSystemError>;
 }
 
