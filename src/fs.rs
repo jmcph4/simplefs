48123bd61defd35c6f476b85480e8ec82b21fce4
jmcph4 <jmcph4@users.noreply.github.com>
2020-01-05 13:56:41 +1000

    Implement inode creation in filesystem module
    This commit implements inode creation via the `create` method for the
    `SimpleFileSystem` type.

--- src/fs.rs
+++ src/fs.rs
@@ -6,6 +6,7 @@ extern crate bincode;
 use serde::{Serialize, Deserialize};
 use math::round;
 use crate::disk::{Disk, BlockDisk, BLOCK_SIZE};
+use std::convert::TryInto;
 
 const MAGIC_NUMBER: u32 = 0xf0f03410;
 const INODES_PER_BLOCK: u32 = 128;
@@ -95,7 +96,49 @@ impl FileSystem<BlockDisk> for SimpleFileSystem<BlockDisk> {
     }
 
     fn create(&mut self) -> Result<usize, FileSystemError> {
-        unimplemented!();
+        let new_inode: Inode = Inode {
+            valid: true as u32,
+            size: 0,
+            direct: [0u32; POINTERS_PER_INODE as usize],
+            indirect: 0
+        };
+
+        let mut curr_inumber: usize = 0;
+
+        let mut inode_block: Vec<u8> = self.inode_block.clone();
+
+        for i in (0..BLOCK_SIZE).step_by(INODE_SIZE) {
+            let inode_bytes: Vec<u8> = inode_block[i..i+INODE_SIZE].to_vec();
+            let valid_field_bytes: &[u8; 4] =
+                &inode_bytes[i..i+4].try_into().unwrap();
+
+            let valid: bool =
+                SimpleFileSystem::slice_to_u32(valid_field_bytes) != 0;
+
+            /* when first free inode encountered, mark it as valid and
+             * return inumber */
+            if !valid {
+                inode_block[i+3] = 1;
+
+                /* write updated inode block to disk */
+                match self.disk.write(1, inode_block) {
+                    Ok(()) => {},
+                    Err(_e) => return Err(FileSystemError::DiskWriteFailure)
+                };
+
+                /* update cached inode block by reading back from disk */
+                self.inode_block = match self.disk.read(1) {
+                    Ok(bytes) => bytes,
+                    Err(_e) => return Err(FileSystemError::DiskReadFailure)
+                };
+
+                return Ok(curr_inumber);
+            }
+
+            curr_inumber += 1;
+        }
+
+        Err(FileSystemError::NoFreeInodes)
     }
 
     fn remove(&mut self, inumber: usize) -> Result<bool, FileSystemError> {
@@ -122,6 +165,13 @@ impl SimpleFileSystem<BlockDisk> {
         for i in 0..disk.size() {
             disk.write(i, vec![0;BLOCK_SIZE]).unwrap();
         }
+    }
+
+    fn slice_to_u32(bytes: &[u8;4]) -> u32 {
+        ((bytes[0] as u32) << 24) |
+            ((bytes[1] as u32) << 16) |
+            ((bytes[2] as u32) << 8) |
+            bytes[3] as u32
     }
 }
 
